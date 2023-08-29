b1f84578855393789a8bd0bb552307f7bc15e989
Yogesh0777 <94049153+Yogesh0777@users.noreply.github.com>
2023-08-29 10:32:33 +0530

    Update main.tf

--- module/main.tf
+++ module/main.tf
@@ -4,13 +4,13 @@ terraform {
 
 provider "azurerm" {
   features {}
-  tenant_id = var.azure_tenant_id
-  subscription_id = var.azure_subscription_id
-  client_id = var.azure_client_id
-  client_secret = var.azure_client_secret
+  tenant_id = var.tenant_id
+  subscription_id = var.subscription_id
+  client_id = var.client_id
+  client_secret = var.client_secret
 }
 
 resource "azurerm_management_group" "management_group" {
-    display_name = var.display_name
-    parent_management_group_id = var.parent_management_group_id
+    display_name = var.display_name_management_group
+    parent_management_group_id = var.parent_ID_management_group
 }
